bb1ce8c6d16b1906159a4120a4ebf5044ee0cffe
mubarakbacancy <mubarak.ansari@bacancy.com>
2023-10-06 11:40:44 +0530

    Here, we create a shared flow. It's a similar flow, but the main difference is that it doesn't provide data that has already been consumed.

--- app/src/main/java/com/example/myapplicationdemoofflow/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationdemoofflow/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.example.myapplicationdemoofflow
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -21,44 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+/*
+* Here, we create a shared flow. It's a similar flow,
+*  but the main difference is that it doesn't provide data that
+* has already been consumed.*/
+
         GlobalScope.launch(Dispatchers.Main) {
-            producer()
-                .onStart {
-                    Log.e(TAG, "onCreate: Consumer start initially", )
-                }
-                .map {
-                    //make some changing on data. like here we make name is uppercase
-                    Note(it.isActive, it.title.uppercase())
-                }
-                .filter {
-                    //here make some filter on data
-                    it.isActive
-                }
-                .collect {
-                    //here only data is show, which one is active state
-                    Log.e(TAG, "onCreate: $it")
-                }
+
         }
     }
 }
 
-fun producer(): Flow<Note> {
-    return flow {
-
-        val list = arrayListOf<Note>(
-            Note(true,"mubarak"),
-            Note(true,"ansari"),
-            Note(false,"happy"),
-        )
-
-        list.forEach {
-            emit(it)
-            delay(1000)
-        }
-    }
-}
-
-data class Note(
-    val isActive: Boolean,
-    val title: String
-)
